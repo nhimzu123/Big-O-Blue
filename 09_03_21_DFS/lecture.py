722112f0385680b70e6c52414fc3f5ca69099cca
Tung Anh <nvta1761999@gmail.com>
2021-10-21 17:30:47 +0700

    Add the lecture.py of DFS Algorithm

---
+++ 09_03_21_DFS/lecture.py
@@ -0,0 +1,5 @@
+"""
+- Depth-First Search (DFS): là thuật toán tìm đường đi từ 1 đỉnh đến các đỉnh khác trên đồ thị vô hướng hoặc có hướng
+KHÔNG CÓ TRỌNG SỐ. Thuật toán DFS luôn tìm được đường đi nhưng KHÔNG CHẮC CHẮN LÀ ĐƯỜNG ĐI NGẮN NHẤT.
+- ĐPT: O(V+E)
+"""
