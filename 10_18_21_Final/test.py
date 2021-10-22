57a9fdcffc844617615d4ae26c7eb12cd91a172b
Tung Anh <nvta1761999@gmail.com>
2021-10-22 14:36:33 +0700

    Edit something dummy, Never mind!

--- 10_18_21_Final/test.py
+++ 10_18_21_Final/test.py
@@ -1,1 +1,19 @@
-print(0)
+projects = [
+    {
+        'id': 1,
+        'description': 'This is a digital newspaper website',
+        'title': 'Digital Newspaper'
+    },
+    {
+        'id': 2,
+        'description': 'This is a e-commerce website',
+        'title': 'E-Commerce'
+    },
+    {
+        'id': 3,
+        'description': 'This is a social network',
+        'title': 'Social Network'
+    },
+]
+
+print(projects[0]['title'])
